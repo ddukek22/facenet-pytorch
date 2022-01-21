Fix Off By One

models/utils/detect_face.py
@@ -270,9 +270,9 @@ def detect_face_scripted(imgs: torch.Tensor, minsize: int, pnet: PNet, rnet: RNe
             boxes = torch.stack([qq1, qq2, qq3, qq4, boxes[:, 4]]).permute(1, 0)
             boxes = rerec(boxes)
             y, ey, x, ex = pad(boxes, w, h)
-            y = y.cpu() - 1
+            y = y.cpu()
             ey = ey.cpu()
-            x = x.cpu() - 1
+            x = x.cpu()
             ex = ex.cpu()
             image_inds_cpu = image_inds.clone().to(torch.device('cpu'))
     
@@ -282,11 +282,11 @@ def detect_face_scripted(imgs: torch.Tensor, minsize: int, pnet: PNet, rnet: RNe
             with nvtx_range('rnet:resample'):
                 im_data = []
                 for k in range(len(y)):
-                    img_k = imgs[image_inds_cpu[k], :, (y[k] - 1):ey[k], (x[k] - 1):ex[k]].unsqueeze(0)
+                    img_k = imgs[image_inds_cpu[k], :, y[k]:ey[k], x[k]:ex[k]]
                     im_data.append(img_k)
             
                 im_data = torch.cat(im_data, dim=0)
-                im_data = interpolate(img_k, (24, 24))
+                im_data = interpolate(im_data, (24, 24))
                 im_data = (im_data - 127.5) * 0.0078125
 
             with nvtx_range('rnet:forward'):
@@ -322,9 +322,8 @@ def detect_face_scripted(imgs: torch.Tensor, minsize: int, pnet: PNet, rnet: RNe
                 image_inds_cpu = image_inds.clone().to(torch.device('cpu'))
                 im_data = []
                 for k in range(len(y)):
-                    if ey[k] > (y[k] - 1) and ex[k] > (x[k] - 1):
-                        img_k = imgs[image_inds_cpu[k], :, (y[k] - 1):ey[k], (x[k] - 1):ex[k]].unsqueeze(0)
-                        im_data.append(imresample(img_k, (48, 48)))
+                    img_k = imgs[image_inds_cpu[k], :, y[k]:ey[k], x[k]:ex[k]].unsqueeze(0)
+                    im_data.append(imresample(img_k, (48, 48)))
                 im_data = torch.cat(im_data, dim=0)
                 im_data = (im_data - 127.5) * 0.0078125
             
@@ -490,7 +489,6 @@ def batched_nms_numpy(boxes, scores, idxs, threshold, method):
 
 
 def pad(boxes: torch.Tensor, w: int, h: int):
-    boxes = boxes.trunc().int()
     x = boxes[:, 0]
     y = boxes[:, 1]
     ex = boxes[:, 2]
@@ -498,21 +496,21 @@ def pad(boxes: torch.Tensor, w: int, h: int):
 
     maxw_mask = ex > w
     maxh_mask = ey > h
-    minw_mask = x < 1
-    minh_mask = y < 1
+    minw_mask = x < 0
+    minh_mask = y < 0
 
     #expand boxes where the standard size crop will be oob and clamped.
     x[maxw_mask] = x[maxw_mask] - (ex[maxw_mask] - w)
     y[maxh_mask] = y[maxh_mask] - (ey[maxh_mask] - h)
-    ex[minw_mask] = ex[minw_mask] + (x[minw_mask] + 1)
-    ey[minh_mask] = ey[minh_mask] + (y[minh_mask] + 1)
+    ex[minw_mask] = ex[minw_mask] - x[minw_mask]
+    ey[minh_mask] = ey[minh_mask] - y[minh_mask]
 
 
-    x[minw_mask] = 1
-    y[minh_mask] = 1
+    x[minw_mask] = 0
+    y[minh_mask] = 0
     ex[maxw_mask] = w
     ey[maxh_mask] = h
-
+    
     return y, ey, x, ex
 
 
@@ -520,10 +518,11 @@ def rerec(bboxA):
     h = bboxA[:, 3] - bboxA[:, 1]
     w = bboxA[:, 2] - bboxA[:, 0]
     
-    l = torch.max(torch.max(w, h))
-    bboxA[:, 0] = bboxA[:, 0] + w * 0.5 - l * 0.5
-    bboxA[:, 1] = bboxA[:, 1] + h * 0.5 - l * 0.5
+    l = torch.floor(torch.max(torch.max(w, h)))
+    bboxA[:, 0] = bboxA[:, 0] + torch.floor(w * 0.5) - torch.floor(l * 0.5)
+    bboxA[:, 1] = bboxA[:, 1] + torch.floor(h * 0.5) - torch.floor(l * 0.5)
     bboxA[:, 2:4] = bboxA[:, :2] + l
+    bboxA = bboxA.floor().int()
 
     return bboxA
 